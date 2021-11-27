70be721a0db1ac079b9f611a58dca3dd587357d9
root <root@ip-172-31-9-92.ec2.internal>
2021-11-27 12:49:15 +0000

    updating code

---
+++ src/test/java/com/simplilearn/example/CaltechDevOps/CaltechDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.example.CaltechDevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaltechDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
